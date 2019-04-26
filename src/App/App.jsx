8cbe7512fcac5b90773d3410465eca1ccb83fd41
noctoid <i@noctoid.com>
2019-04-25 22:03:49 -0700

    changed HomePage.jsx layout

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,56 +1,55 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import {Router, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-import { history } from '../_helpers';
-import { alertActions } from '../_actions';
-import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
-import { LoginPage } from '../LoginPage';
-import { ProfilePage } from "../ProfilePage";
+import {history} from '../_helpers';
+import {alertActions} from '../_actions';
+import {PrivateRoute} from '../_components';
+import {HomePage} from '../HomePage';
+import {LoginPage} from '../LoginPage';
+import {ProfilePage} from "../ProfilePage";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { dispatch } = this.props;
-        history.listen((location, action) => {
-            // clear alert on location change
-            dispatch(alertActions.clear());
-        });
-    }
+    const {dispatch} = this.props;
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }
 
-    render() {
-        const { alert } = this.props;
-        return (
-            <div className="jumbotron">
-                <div className="container">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                        <Router history={history}>
-                            <div>
-                                <PrivateRoute exact path="/" component={HomePage} />
-                                <Route path="/login" component={LoginPage} />
-                                <Route path="/profile" component={ProfilePage} />
-                                <Route path="/114514" render={()=>
-                                  <iframe src="http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"></iframe>} />
-                            </div>
-                        </Router>
-                    </div>
-                </div>
+  render() {
+    const {alert} = this.props;
+    return (
+      <div className="jumbotron">
+        <div className="container">
+
+          {alert.message &&
+            <div className={`alert ${alert.type}`}>{alert.message}</div>
+          }
+          <Router history={history}>
+            <div>
+              <PrivateRoute exact path="/" component={HomePage}/>
+              <Route path="/login" component={LoginPage}/>
+              <Route path="/profile" component={ProfilePage}/>
+              <Route path="/114514" render={() =>
+                <iframe src="http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"></iframe>}/>
             </div>
-        );
-    }
+          </Router>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { alert } = state;
-    return {
-        alert
-    };
+  const {alert} = state;
+  return {
+    alert
+  };
 }
 
 const connectedApp = connect(mapStateToProps)(App);
-export { connectedApp as App }; 
+export {connectedApp as App};
--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -20,35 +20,46 @@ class HomePage extends React.Component {
 
     return (
       <Router>
-        <div className="col-md-8">
-          <h2>NekoGram!</h2>
-          {/*My Posts*/}
-          <Route path="/login" component={LoginPage}/>
-          <Route path="/" exact render={() =>
-            <div>
-              <h3>{profile.items && profile.items.displayName} My Posts</h3>
-              <div>
-                {myposts.loading && <em>Loading Posts...</em>}
-                {myposts.error && <span className="text-danger">ERROR: {myposts.error}</span>}
-                {myposts.items &&
-                <div>
-                  {myposts.items.map((i, index1) =>
-                    <Posting key={index1} posting={i}/>
-                  )}
-                </div>
-                }
-              </div>
+        <div>
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/login"> Logout </Link>|
+              <Link to="/"> Home </Link>|
+              <Link to="/profile"> Profile </Link>|
+              <Link to="/discover"> Discover </Link>|
+              <Link to="/feed"> Feed </Link>
             </div>
-          }/>
-          {/*Profile Page*/}
-          <Route path="/profile" component={ProfilePage}/>
-          <p>
-            <Link to="/login"> Logout </Link>|
-            <Link to="/"> Home </Link>|
-            <Link to="/profile"> Profile </Link>|
-            <Link to="/discover"> Discover </Link>|
-            <Link to="/feed"> Feed </Link>
-          </p>
+          </div>
+          <div>
+            <h2>NekoGram!</h2>
+          </div>
+          {/*<div className="container">*/}
+            <div className="row">
+              <div className="col-md-4 col-sm-12">
+                <h3>{profile.items && profile.items.displayName}</h3>
+                <h5>@{profile.items && profile.items.username}</h5>
+
+              </div>
+              {/*My Posts*/}
+              <Route path="/login" component={LoginPage}/>
+              <Route path="/" exact render={() =>
+                <div className="col-md-8 col-sm-12">
+                  {myposts.loading && <em>Loading Posts...</em>}
+                  {myposts.error && <span className="text-danger">ERROR: {myposts.error}</span>}
+                  {myposts.items &&
+                  <div>
+                    {myposts.items.map((i, index1) =>
+                      <Posting key={index1} posting={i}/>
+                    )}
+                  </div>
+                  }
+                </div>
+              }/>
+              {/*Profile Page*/}
+              <Route path="/profile" component={ProfilePage}/>
+            </div>
+
+          {/*</div>*/}
 
         </div>
       </Router>
