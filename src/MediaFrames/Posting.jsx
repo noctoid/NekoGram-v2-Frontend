25a7c518b29b2007bd06a14deb28c9ba5b062f7c
noctoid <i@noctoid.com>
2019-05-03 02:02:43 -0700

    display like Posting

--- src/MediaFrames/Posting.jsx
+++ src/MediaFrames/Posting.jsx
@@ -23,28 +23,61 @@ class Posting extends Component {
     let commentCount = posting.comments.length;
     let repostCount = posting.repost.length;
 
-    return (
-      <div className="panel panel-default posting">
-        <div className="row">
-          <div className="col-md-1">
-            {/*<p>pid: {posting.pid}</p>*/}
-            <img src={posting.avatarUrl} className=" avatar-sm"/>
-          </div>
-          <div className="col-md-11">
-              <div className="col-md-12">
-                <h4>{posting.displayName} @{posting.username}</h4>
-                <hr />
-                <p>{posting.content.txt}</p>
-                {postingMedia}
 
-                <LikeButton pid={posting.pid}/>
-                <DeleteButton pid={posting.pid}/>
-                <p>Comments: {commentCount} Repost: {repostCount} Likes: {likesCount}</p>
+    let postingContent = "";
+
+    switch (posting.type) {
+      case "posting":
+        return (
+          <div className="panel panel-default posting">
+            <div className="row">
+              <div className="col-md-1">
+                {/*<p>pid: {posting.pid}</p>*/}
+                <img src={posting.avatarUrl} className="avatar-sm"/>
+              </div>
+              <div className="col-md-11">
+                <div className="col-md-12">
+                  <h4>{posting.displayName} @{posting.username} says</h4>
+                  <hr/>
+                  <p>{posting.content.txt}</p>
+                  {postingMedia}
+
+                  <LikeButton pid={posting.pid}/>
+                  <DeleteButton pid={posting.pid}/>
+                  <p>Comments: {commentCount} Repost: {repostCount} Likes: {likesCount}</p>
+                </div>
               </div>
             </div>
-        </div>
-      </div>
-    );
+          </div>
+        );
+      case "like":
+        return (
+          <div className="panel panel-default posting">
+            <div className="row">
+              <div className="col-md-1">
+                {/*<p>pid: {posting.pid}</p>*/}
+                <img src={posting.avatarUrl} className="avatar-sm"/>
+              </div>
+              <div className="col-md-11">
+                <div className="col-md-12">
+                  <h4>{posting.displayName} @{posting.username} liked</h4>
+                  <hr/>
+                  <p>{posting.root}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      case "repost":
+        return (
+          <div className="panel panel-default posting">
+            {posting.type} {posting.root}
+          </div>
+        )
+    }
+
+
+
   }
 }
 
